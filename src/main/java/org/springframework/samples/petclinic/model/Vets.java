8aaad6605452d498e8bd70f2d03ac146f86da51d
Divya <glishasunil@gmail.com>
2024-03-23 20:50:40 +0000

    removed error

--- src/main/java/org/springframework/samples/petclinic/model/Vets.java
+++ src/main/java/org/springframework/samples/petclinic/model/Vets.java
@@ -15,18 +15,11 @@
  */
 package org.springframework.samples.petclinic.model;
 
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-
-/**
- * Simple domain object representing a list of veterinarians. Mostly here to be used for the 'vets' {@link
- * org.springframework.web.servlet.view.xml.MarshallingView}.
- *
- * @author Arjen Poutsma
- */
 @XmlRootElement
 public class Vets {
 
@@ -39,5 +32,5 @@ public class Vets {
         }
         return vetList;
     }
-
 }
+
--- src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaPetRepositoryImpl.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaPetRepositoryImpl.java
@@ -17,8 +17,10 @@ package org.springframework.samples.petclinic.repository.jpa;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+
 
 import org.springframework.samples.petclinic.model.Pet;
 import org.springframework.samples.petclinic.model.PetType;
